290dbf43b3344ca75441790911b1adc6343e3fe0
Alexander Hoppe <alexander.hoppe@students.olin.edu>
2015-11-13 13:37:15 -0500

    added broken Fdrag function

---
+++ Fdrag.m
@@ -0,0 +1,6 @@
+% function drag = Fdrag(v, h)
+% This function returns the drag force on a projectile as a function of its
+% velocity and its radius (distance from origin)
+function drag = Fdrag(v, r)
+    drag = Cd(v, r) * density(;
+end
