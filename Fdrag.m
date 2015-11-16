19b4cb2e301829f853dbd097705a418c6d6442e2
Alexander Hoppe <alexander.hoppe@students.olin.edu>
2015-11-16 13:34:44 -0500

    Fdrag, still incomplete

--- Fdrag.m
+++ Fdrag.m
@@ -2,5 +2,5 @@
 % This function returns the drag force on a projectile as a function of its
 % velocity and its radius (distance from origin)
 function drag = Fdrag(v, r)
-    drag = Cd(v, r) * density(;
+    drag = Cd(v, r) * density( ;
 end
