8fdcec09c97fc024286be703e6dc52595a3668ea
ppfenninger <paige.pfennninger@students.olin.edu>
2015-11-18 11:31:52 -0500

    function to stop orbitFunc when projectile hits earth surface

---
+++ orbitEvent.m
@@ -0,0 +1,11 @@
+function [value, isTerminal, direction] = eventFunc(~, S) 
+    X = S(1);
+    Y = S(2);
+    
+    radiusEarth = 6371000; % meters
+    
+    radius = sqrt(X^2 + Y^2); 
+    value = radius - radiusEarth; 
+    isTerminal = 1;
+    direction = 0; 
+end 
