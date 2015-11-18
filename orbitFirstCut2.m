a4107b8b56db0c89e3de6ce208f19f69faef8993
ppfenninger <paige.pfennninger@students.olin.edu>
2015-11-18 11:32:41 -0500

    cartesian first cut model

---
+++ orbitFirstCut2.m
@@ -0,0 +1,36 @@
+function orbitFirstCut2(angle, velocity)
+    radiusEarth = 6371000; % meters
+
+    initialVelocity = velocity; % m /s  
+    launchAngle = angle; % degrees
+    launchAngleRad = pi* launchAngle / 180; % radians  
+
+    initialX = 0; %meters
+    initialY = radiusEarth + 1; %meters
+
+    initialVx = initialVelocity*cos(launchAngleRad); % m/s 
+    initialVy = initialVelocity*sin(launchAngleRad); % m/s
+
+    initial = [initialX; initialY; initialVx; initialVy]; 
+
+    initialTime = 0; %seconds
+    finalTime = 100*60; %seconds
+
+    options = odeset('events', @orbitEvent); 
+
+    [T, S] = ode45(@orbitFlow2, [initialTime, finalTime], initial, options);
+
+    X = S(:, 1); 
+    Y = S(:, 2); 
+    
+    th = 0:pi/50:2*pi;
+    xunit = radiusEarth * cos(th);
+    yunit = radiusEarth * sin(th);
+    plot(xunit, yunit);
+
+    hold on
+    axis equal
+    
+    %% plot output
+    comet(X, Y); 
+end
