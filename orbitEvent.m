179791fbb8056256d5d584860af731e37991e55a
ppfenninger <paige.pfennninger@students.olin.edu>
2015-11-19 11:02:21 -0500

    "event function for cartesian plot"

--- orbitEvent.m
+++ orbitEvent.m
@@ -1,10 +1,8 @@
 function [value, isTerminal, direction] = orbitEvent(~, S) 
-    X = S(1);
-    Y = S(2);
+    radius = S(2);
     
     radiusEarth = 6371000; % meters
     
-    radius = sqrt(X^2 + Y^2); 
     value = radius - radiusEarth; 
     isTerminal = 1;
     direction = 0; 
