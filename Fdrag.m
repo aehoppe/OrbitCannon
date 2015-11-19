cc24590aa6a6acc321f5f27ee5eb8f1dca5b8708
Alexander Hoppe <alexander.hoppe@students.olin.edu>
2015-11-19 12:03:02 -0500

    fixed Fdrag

--- Fdrag.m
+++ Fdrag.m
@@ -1,7 +1,7 @@
-% function drag = fDrag(v, h)
+% function drag = Fdrag(v, h)
 % This function returns the drag force on a projectile as a function of its
 % velocity and its radius (distance from origin)
-function drag = fDrag(v, r)
+function drag = Fdrag(v, r)
     area = 0.001137; %m^2
     drag = (Cd(v, r) * density(r) * v^2 * area)/2;
 end
