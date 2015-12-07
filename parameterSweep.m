3c3ee54966be4c76ebc18fed018fe22d9097d633
Alexander Hoppe <alexander.hoppe@students.olin.edu>
2015-12-06 20:22:23 -0500

    added incomplete targeting function and edited parameter sweep

--- parameterSweep.m
+++ parameterSweep.m
@@ -1,21 +1,23 @@
 %% Set some shit up
 clf
 hold on
-angleEnd = 70;
+angleEnd = 90;
 velocityEnd = 210;
 
+results = [-1, -1, -1];
+
 %% Plot Earth
-axis equal;
-hold on
-radiusEarth = 6371000;
-th = 0:pi/50:2*pi;
-xunit = radiusEarth * cos(th);
-yunit = radiusEarth * sin(th);
-plot(xunit, yunit, 'g');
+% axis equal;
+% hold on
+% radiusEarth = 6371000;
+% th = 0:pi/50:2*pi;
+% xunit = radiusEarth * cos(th);
+% yunit = radiusEarth * sin(th);
+% plot(xunit, yunit, 'g');
 
 %% Parameter Sweep
 for i = 1:angleEnd
-    angle = i + 9;
+    angle = i;
     disp(i/(angleEnd)*100);
 
     for j = 1:velocityEnd
@@ -23,30 +25,34 @@ for i = 1:angleEnd
         
         h = orbitFirstCut2(angle, velocity);
         
-        X(i, j) = h(1); 
-        Y(i, j) = h(2); 
+        x = h(1);
+        y = h(2);
+        
+        X(i, j) = x; 
+        Y(i, j) = y; 
         escape(i,j) = h(3); 
         
         hold on;
         if escape(i,j) == 0  
-            plot(X(i,j), Y(i,j), 'r*'); 
-        else 
-%             plot(X(i,j), Y(i,j), 'b*');
+            if(x < 0)
+                theta = atand(y/x);
+            else
+                theta = 180 - atand(y/x);
+            end
+            results = [results; [angle, velocity, theta]];
         end
-        velocitySave(i,j) = velocity;
-        angleSave(i,j) = angle;
-
     end
 end
 
 %% PLOT EARTH AGAAAAAAIN  
-        
-hold on
-radiusEarth = 6371000;
-th = 0:pi/50:2*pi;
-xunit = radiusEarth * cos(th);
-yunit = radiusEarth * sin(th);
-plot(xunit, yunit, 'g');
-axis([-8e+6 8e+6 -8e+6 8e+6]);
+%         
+% hold on
+% axis equal
+% radiusEarth = 6371000;
+% th = 0:pi/50:2*pi;
+% xunit = radiusEarth * cos(th);
+% yunit = radiusEarth * sin(th);
+% plot(xunit, yunit, 'g');
+% axis([-8e+6 8e+6 -8e+6 8e+6]);
 
 
