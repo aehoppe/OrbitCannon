7a6d909b0a638f646a5ee1d9f2cb61a7ac9e9887
ppfenninger <paige.pfennninger@students.olin.edu>
2015-12-08 13:59:02 -0500

    more poster stuff

--- pressureSet.m
+++ pressureSet.m
@@ -2,4 +2,7 @@ x = linspace(0, 44);
 y = 101325*(1 - (x.*6.5)./288.15).^((9.8*28.964)/(8.314*6.5));
 
 clf
-plot(x, y);
+linewidth = 2;
+plot(x, y, 'm', 'LineWidth', linewidth);
+xlabel('Altitude (Kilometers)');
+ylabel('Pressure (Pascals)'); 
